ce464e07ebc7c8ff7e2da7b797a4cd0d0d01b4f4
kasztanka <m.lipowska@onet.pl>
2017-03-24 22:55:02 +0100

    Create team show view

--- ballegro/views.py
+++ ballegro/views.py
@@ -5,3 +5,8 @@ from .models import Team
 def root(request):
     teams = Team.objects.all()
     return render(request, 'ballegro/root.html', {'teams': teams})
+
+def team_show(request, team_name):
+    team_name = team_name.replace('_', ' ')
+    team = get_object_or_404(Team, name=team_name)
+    return render(request, 'ballegro/team_show.html', {'team': team})
