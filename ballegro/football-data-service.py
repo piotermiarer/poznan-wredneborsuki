392ddb04c9270dced42397f4973a8b8528cfed6f
piotermiarer <piotr@atopik.pl>
2017-03-25 00:30:33 +0100

    Refactor football-data-service.py

--- ballegro/football-data-service.py
+++ ballegro/football-data-service.py
@@ -2,26 +2,24 @@ import http.client
 import json
 
 connection = http.client.HTTPConnection('api.football-data.org')
-headers = { 'X-Auth-Token': '9df97b651e4244bbb8da6d50a6156d2b',
-            'X-Response-Control': 'minified' }
+headers = {
+    'X-Auth-Token': '9df97b651e4244bbb8da6d50a6156d2b',
+    'X-Response-Control': 'minified'
+}
 # should get next 7 fixtures
-connection.request( 'GET', '/v1/fixtures', None, headers )
+connection.request('GET', '/v1/fixtures', None, headers)
 upcoming_fixtures = json.loads(connection.getresponse().read().decode())
 # get some teams
-leages_with_teams = []
-connection.request( 'GET', '/v1/competitions/', None, headers )
-leage_table = json.loads(connection.getresponse().read().decode())
-i=0
-for league in leage_table:
-    leages_with_teams.append({})
-    leages_with_teams[i]["name"] = league["caption"]
-    leages_with_teams[i]["teams"] = []
+leagues_with_teams = []
+connection.request('GET', '/v1/competitions/', None, headers)
+league_list = json.loads(connection.getresponse().read().decode())
+for league in league_list:
+    leagues_with_teams.append({})
+    leagues_with_teams[-1]["name"] = league["caption"]
+    leagues_with_teams[-1]["teams"] = []
     url = '/v1/competitions/'+ str(league["id"]) + '/teams'
-    connection.request( 'GET', url, None, headers )
-    teams_in_leage = json.loads(connection.getresponse().read().decode())
-    for team in teams_in_leage["teams"]:
+    connection.request('GET', url, None, headers)
+    teams_in_league = json.loads(connection.getresponse().read().decode())
+    for team in teams_in_league["teams"]:
         team = {"name": team["name"], "crest_url": team["crestUrl"]}
-        leages_with_teams[i]["teams"].append(team)
-    i += 1
-
-print(leages_with_teams)
+        leagues_with_teams[-1]["teams"].append(team)
