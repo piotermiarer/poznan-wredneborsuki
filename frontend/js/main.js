7154de32af9514959e02f9b4dadc6ba3f3b071f8
Tomke Jósiak <tomek.josiak@gmail.com>
2017-03-25 09:51:10 +0100

    Executing code based on specific route

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,11 @@
 
 const teamShow = require('./team-show');
+const offers = require('./offers');
 
 document.addEventListener('DOMContentLoaded', () => {
-    teamShow();
+    const route = document.querySelector('.main').getAttribute('id');
+    switch (route) {
+        case 'team-show': teamShow(); break;
+        case 'offers': offers(); break;
+    }
 });
