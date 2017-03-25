c76913d1f766b5e51170ad2adb57b4ccce6721d1
piotermiarer <piotr@atopik.pl>
2017-03-25 04:03:07 +0100

    Refactor add_teams_to_database

--- add_teams_to_database.py
+++ add_teams_to_database.py
@@ -5,42 +5,59 @@ import requests
 from urllib.parse import unquote
 from PIL import Image
 
+# do some django magic to be able to modify database
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 import django
 django.setup()
-from ballegro.models import Team
+
+from ballegro.models import Team, League
 
 
-def change_photo(path):
+def main():
+    """
+    Adding leagues and teams to database.
+    It includes downloading images and resizing them.
+    """
+    with open('leagues.json', 'r', encoding="utf8") as f:
+        leagues = json.load(f)
+
+    add_all_leagues = False
+    if len(sys.argv) < 2:
+        add_all_leagues = True
+
+    for league in leagues:
+        if add_all_leagues or league['name'] == sys.argv[1]:
+            new_league = League(name=league['name'])
+            new_league.save()
+            for team in league['teams']:
+                url = team['crest_url']
+                response = requests.get(url)
+                filename = url.split('/')[-1]
+                image_filename = 'static/images/' + unquote(filename)
+                with open('ballegro/' + image_filename, 'wb') as f:
+                    f.write(response.content)
+                if not image_filename.endswith('.svg'):
+                    resize_photo('ballegro/' + image_filename)
+                new_team = Team(name=team['name'], image=image_filename, league=new_league)
+                new_team.save()
+
+
+def resize_photo(path):
     productImage = Image.open(path)
     width, height = productImage.size
     photoNeedsChange = False
-    if width > height and width > 200:
-        proportion = width / 200
-        productImage = productImage.resize((200, int(height / proportion)), Image.ANTIALIAS)
+    MAX_SIZE = 200
+    if width > height and width > MAX_SIZE:
+        proportion = width / MAX_SIZE
+        productImage = productImage.resize((MAX_SIZE, int(height / proportion)), Image.ANTIALIAS)
         photoNeedsChange = True
-    elif height >= width and height > 200:
-        proportion = height / 200
-        productImage = productImage.resize((int(width / proportion), 200), Image.ANTIALIAS)
-        photoNeedsChange = True
-    if width == 200 and height == 200:
+    elif height >= width and height > MAX_SIZE:
+        proportion = height / MAX_SIZE
+        productImage = productImage.resize((int(width / proportion), MAX_SIZE), Image.ANTIALIAS)
         photoNeedsChange = True
     if photoNeedsChange:
         productImage.save(path, quality=90)
 
-with open('leagues.json', 'r', encoding="utf8") as f:
-    leagues = json.load(f)
 
-for league in leagues:
-    if league['name'] == sys.argv[1]:
-        for team in league['teams']:
-            url = team['crest_url']
-            response = requests.get(url)
-            filename = url.split('/')[-1]
-            image_filename = 'static/images/' + unquote(filename)
-            with open('ballegro/' + image_filename, 'wb') as f:
-                f.write(response.content)
-            if not image_filename.endswith('.svg'):
-                change_photo('ballegro/' + image_filename)
-            new_team = Team(name=team['name'], image=image_filename)
-            new_team.save()
+if __name__ == '__main__':
+    main()
